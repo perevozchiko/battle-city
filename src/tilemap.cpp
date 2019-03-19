6ad278e12d91277e7cd6a2a5d72674bc5d5edc2b
perevozchikov <perevozchikov.andrey@gmail.com>
2019-03-19 06:29:35 +0400

    мелкие правки

--- src/tilemap.cpp
+++ src/tilemap.cpp
@@ -1,12 +1,10 @@
 #include "include/tilemap.h"
 
 TileMap::TileMap(Conf::Tile _type) :
-    Entity ({0, 0}, Conf::SizeTileMap, {0, 0}),
+    Entity ({0, 272}, Conf::SizeTileMap, {0, 0}),
     type(_type)
 {
-   getSprite().setTextureRect(sf::IntRect(0, 272, 16, 16));
-   getSprite().setOrigin(8, 8);
-   getSprite().setPosition(250, 250);
+
 }
 
 void TileMap::update(const sf::Time &elapsedTime)
